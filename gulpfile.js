7962d65d51af3de8083bf8f38f3e5d1d25950f4d
Sean Groff <sgroff04@gmail.com>
2016-03-09 19:31:09 -0600

    restructured project and added Jade

--- gulpfile.js
+++ gulpfile.js
@@ -4,32 +4,42 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 const babel = require('gulp-babel');
+const jade = require('gulp-jade');
+
+gulp.task('jade', () => {
+   return gulp.src('src/templates/**/*.jade')
+      .pipe(jade())
+      .pipe(gulp.dest('dist'))
+      .pipe(browserSync.reload({
+         stream: true
+      }));
+});
 
 gulp.task('sass', () => {
-   return gulp.src('src/scss/**/*.scss')
+   return gulp.src('src/sass/**/*.scss')
       .pipe(sass())
-      .pipe(gulp.dest('src/css'))
+      .pipe(gulp.dest('dist/css'))
       .pipe(browserSync.reload({
          stream: true
       }));
 });
 
 gulp.task('babel', () => {
-   return gulp.src('src/scripts/**/*.js')
+   return gulp.src('src/js/**/*.js')
       .pipe(babel())
-      .pipe(gulp.dest('dist'));
+      .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('browserSync', () => {
    browserSync.init({
       server: {
-         baseDir: 'src'
+         baseDir: 'dist'
       },
    });
 });
 
-gulp.task('watch', ['browserSync', 'sass', 'babel'], () => {
-   gulp.watch('src/scss/**/*.scss', ['sass']);
-   gulp.watch('src/*.html', browserSync.reload);
+gulp.task('watch', ['browserSync', 'jade', 'sass', 'babel'], () => {
+   gulp.watch('src/sass/**/*.scss', ['sass']);
+   gulp.watch('src/templates/**/*.jade', ['jade']);
    gulp.watch('src/scripts/**/*.js', browserSync.reload);
 });
